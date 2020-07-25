433da1af19a85231adeb820f3caf3567f5f956e7
Charlie Hay <robertcharleshay@outlook.com>
2020-07-25 18:42:17 -0400

    feat(tm): rules and social images

--- hosting/src/ui/atoms/SEO/SEO.component.tsx
+++ hosting/src/ui/atoms/SEO/SEO.component.tsx
@@ -2,10 +2,9 @@ import React, { useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import { useLocation } from '@reach/router'
 import { ga } from 'services/firebase'
-import { formatMessage } from 'ui/language/translations'
 
-const SEO = ({ page, crawl }) => {
-  const { origin = 'https://telus-me.web.app', pathname } = useLocation()
+const SEO = () => {
+  const { origin = 'https://telus-me.web.app' } = useLocation()
 
   useEffect(() => {
     if (origin === 'https://telus-me.web.app') ga().logEvent(`page_view`)
@@ -13,36 +12,39 @@ const SEO = ({ page, crawl }) => {
 
   return (
     <Helmet>
-      <html lang={formatMessage('active-lang')} />
+      <html lang="en" />
       <base href={origin} />
 
       {/* Primary Meta Tags */}
-      <title>{formatMessage(`${page}-seo-title`)}</title>
-      <meta name="title" content={formatMessage(`${page}-seo-title`)} />
-      <meta name="description" content={formatMessage(`${page}-seo-description`)} />
-      <link rel="canonical" href={`https://telus-me.web.app${pathname}`} />
-      <meta name="robots" content={crawl ? 'index, follow' : 'noindex, follow'} />
-      <meta
-        name="keywords"
-        content="Telus ME"
-      />
+      <title>TELUS Me | How was your day?</title>
+      <meta name="title" content="TELUS Me | How was your day?" />
+      <meta name="description" content="Describe how your day went and track your satisfaction at work over time!" />
+      <link rel="canonical" href="https://telus-me.web.app" />
+      <meta name="robots" content="noindex, follow" />
+      <meta name="keywords" content="Telus ME" />
       <meta name="author" content="Charlie Hay" />
       <meta name="publisher" content="Charlie Hay" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={`https://telus-me.web.app${pathname}`} />
-      <meta property="og:title" content={formatMessage(`${page}-seo-title`)} />
-      <meta property="og:description" content={formatMessage(`${page}-seo-description`)} />
+      <meta property="og:url" content="https://telus-me.web.app" />
+      <meta property="og:title" content="TELUS Me | How was your day?" />
+      <meta
+        property="og:description"
+        content="Describe how your day went and track your satisfaction at work over time!"
+      />
       <meta property="og:image" content="https://telus-me.web.app/social/og-image.jpg" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={`https://telus-me.web.app${pathname}`} />
-      <meta property="twitter:title" content={formatMessage(`${page}-seo-title`)} />
-      <meta property="twitter:description" content={formatMessage(`${page}-seo-description`)} />
-      <meta property="twitter:image" content={`https://telus-me.web.app/social/og-image.jpg`} />
+      <meta property="twitter:url" content="https://telus-me.web.app" />
+      <meta property="twitter:title" content="TELUS Me | How was your day?" />
+      <meta
+        property="twitter:description"
+        content="Describe how your day went and track your satisfaction at work over time!"
+      />
+      <meta property="twitter:image" content="https://telus-me.web.app/social/og-image.jpg" />
 
       {/* Favicons */}
       <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png" />
