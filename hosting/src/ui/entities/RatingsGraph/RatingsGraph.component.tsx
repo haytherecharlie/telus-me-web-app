0b0c7e78b0f2ea363725f545401e2d1657d2ed9f
Charlie Hay <robertcharleshay@outlook.com>
2020-08-11 15:39:58 -0400

    feat(00): gatsby window bull

--- hosting/src/ui/entities/RatingsGraph/RatingsGraph.component.tsx
+++ hosting/src/ui/entities/RatingsGraph/RatingsGraph.component.tsx
@@ -1,28 +1,31 @@
+const { BarChart } = typeof window !== 'undefined' ? require('react-charts-d3') : { BarChart: undefined }
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { BarChart } from 'react-charts-d3'
 import * as S from './RatingsGraph.style'
 
 const RatingsGraph = () => {
   const { personal } = useSelector(s => s.ratings)
+
   return (
     <S.Wrapper>
-      <BarChart
-        height={150}
-        width={600}
-        barSpacing={0.15}
-        axisConfig={{
-          showXAxis: true,
-          showXAxisLabel: true,
-          xLabel: 'Date',
-          xLabelPosition: 'right',
-          showYAxis: true,
-          showYAxisLabel: true,
-          yLabel: 'Rating',
-          yLabelPosition: 'top'
-        }}
-        data={[{ key: 'personal', values: personal.data.slice(0, 5) }]}
-      />
+      {BarChart && (
+        <BarChart
+          height={150}
+          width={600}
+          barSpacing={0.15}
+          axisConfig={{
+            showXAxis: true,
+            showXAxisLabel: true,
+            xLabel: 'Date',
+            xLabelPosition: 'right',
+            showYAxis: true,
+            showYAxisLabel: true,
+            yLabel: 'Rating',
+            yLabelPosition: 'top'
+          }}
+          data={[{ key: 'personal', values: personal.data.slice(0, 5) }]}
+        />
+      )}
     </S.Wrapper>
   )
 }
